c763634315e07ad9df0779d070f20d18b77c6af0
liuyuzhe <15313482685@163.com>
2021-05-28 17:04:24 +0800

    气泡指示器顶部和底部类型的显示
    Signed-off-by: liuyuzhe <15313482685@163.com>

--- lib/widget/bubble_simple_widget.dart
+++ lib/widget/bubble_simple_widget.dart
@@ -1,6 +1,10 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:hy_flutter_simple/widget/base_stateful_widget.dart';
 
+/// 气泡指示器
+/// TextButton.icon 当里面的文字越界了，无法使用expand包裹来解决；只能拆开单独使用text
 class BubbleSimpleWidget extends BaseStatefulWidget {
   @override
   State<BaseStatefulWidget> createState() => BubbleSimpleState();
@@ -8,6 +12,8 @@ class BubbleSimpleWidget extends BaseStatefulWidget {
 
 class BubbleSimpleState extends BaseStatefulState {
   GlobalKey blueKey = GlobalKey();
+  GlobalKey redKey = GlobalKey();
+  GlobalKey greenKey = GlobalKey();
 
   @override
   Widget buildWidget(BuildContext context) {
@@ -16,42 +22,97 @@ class BubbleSimpleState extends BaseStatefulState {
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height,
       child: Stack(
-        children: [
-          Container(
-            width: 100,
-            height: 100,
-            color: Colors.blue,
-            child: TextButton(
-                child: Text("ss"),
-                key: blueKey,
-                onPressed: () {
-                  showDialog(
-                      context: context,
-                      builder: (context) {
-                        var size = (blueKey.currentContext!.findRenderObject()
-                                as RenderBox)
-                            .size;
-
-                        return BubbleDialog(BubbleBuilder(
-                            (getX(blueKey) + size.width / 2).toDouble(),
-                            (getY(blueKey) + size.height - getStatusBarHeight())
-                                .toDouble(),
-                            text: "simple1"));
-                      });
-                }),
-          ),
-          Positioned(
-              left: MediaQuery.of(context).size.width / 2,
-              child: Container(
-                color: Colors.green,
-                child: TextButton(
-                  child: Text("FlatButton"),
-                  onPressed: () => print("FlatButton Click"),
-                ),
-              ))
-        ],
+        children: [blueButton(context), greenButton(context), redButton()],
       ),
     );
+  }
+
+  Positioned greenButton(BuildContext context) {
+    return Positioned(
+        left: MediaQuery.of(context).size.width / 2,
+        child: Container(
+          key: greenKey,
+          color: Colors.green,
+          child: TextButton(
+            child: Text("simple2"),
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    var size = (greenKey.currentContext!.findRenderObject()
+                            as RenderBox)
+                        .size;
+
+                    return BubbleDialog(BubbleBuilder(
+                        (getX(greenKey) + size.width / 2).toDouble(),
+                        (getY(greenKey) + size.height - getStatusBarHeight())
+                            .toDouble(),
+                        text: "simple2"));
+                  });
+            },
+          ),
+        ));
+  }
+
+  Container blueButton(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      child: TextButton(
+          child: Text("simple1"),
+          key: blueKey,
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (context) {
+                  var size =
+                      (blueKey.currentContext!.findRenderObject() as RenderBox)
+                          .size;
+
+                  return BubbleDialog(BubbleBuilder(
+                      (getX(blueKey) + size.width / 2).toDouble(),
+                      (getY(blueKey) + size.height - getStatusBarHeight())
+                          .toDouble(),
+                      text: "simple1"));
+                });
+          }),
+    );
+  }
+
+  redButton() {
+    var screen = MediaQuery.of(context).size;
+    var buttonTheme = Theme.of(context).buttonTheme;
+    return Positioned(
+        left: screen.width / 2 - buttonTheme.minWidth / 2,
+        top: (screen.height -
+                kToolbarHeight -
+                MediaQuery.of(context).padding.top) /
+            2,
+        child: Container(
+            key: redKey,
+            color: Colors.red,
+            width: Theme.of(context).buttonTheme.minWidth,
+            height: Theme.of(context).buttonTheme.height,
+            child: TextButton(
+              child: Text("simple3"),
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (context) {
+                      var size = (redKey.currentContext!.findRenderObject()
+                              as RenderBox)
+                          .size;
+
+                      return BubbleDialog(BubbleBuilder(
+                          (getX(redKey) + size.width / 2).toDouble(),
+                          (getY(redKey) -
+                                  BubbleBuilder.BUBBLE_HEIGHT -
+                                  getStatusBarHeight())
+                              .toDouble(),
+                          text: "simple2",
+                          arrowLocation: ArrowLocation.BOTTOM));
+                    });
+              },
+            )));
   }
 }
 
@@ -79,38 +140,103 @@ class BubbleDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print("x= ${builder.x}");
+    Size size = MediaQuery.of(context).size;
+
     var x = builder.x;
+    if (top_b()) {
+      x = builder.x - builder.bubbleWidth / 2;
+    }
+
+    if (x < 0) {
+      //目标靠左边屏幕
+      x = 0;
+    } else if ((x + builder.bubbleWidth) > size.width) {
+      //目标过于靠右边
+      x = size.width - builder.bubbleWidth;
+    }
+
+    builder.arrowPosition = calculateArrowPosition(x);
+
+    var margin = EdgeInsets.zero;
     if (builder.arrowLocation == ArrowLocation.TOP) {
-      x = 0.0;
+      margin = EdgeInsets.only(top: builder.arrowHeight, left: 5, right: 5);
+    } else if (builder.arrowLocation == ArrowLocation.BOTTOM) {
+      margin = EdgeInsets.only(left: 5, right: 5, bottom: builder.arrowHeight);
     }
 
     return Scaffold(
       backgroundColor: Colors.transparent,
-      body: Container(
-        child: InkWell(
-          onTap: () => Navigator.of(context).pop(),
-          child: Container(
-            alignment: Alignment.centerLeft,
-            width: builder.bubbleWidth,
-            height: builder.bubbleHeight,
-            color: Colors.purple,
-            child: CustomPaint(
-              size: Size(builder.bubbleWidth, builder.bubbleHeight),
-              painter: BubblePainter(builder),
-            ),
-            margin: EdgeInsets.only(left: x, top: builder.y),
+      body: GestureDetector(
+        behavior: HitTestBehavior.translucent,
+        onTap: () => Navigator.of(context).pop(),
+        child: Container(
+          alignment: Alignment.centerLeft,
+          width: builder.bubbleWidth,
+          height: builder.bubbleHeight,
+          child: Stack(
+            children: [
+              CustomPaint(
+                size: Size(builder.bubbleWidth, builder.bubbleHeight),
+                painter: BubblePainter(builder),
+              ),
+              Center(
+                child: Container(
+                  color: Colors.green.shade50,
+                  margin: margin,
+                  width: builder.bubbleWidth,
+                  height: builder.bubbleHeight,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(Icons.send),
+                      SizedBox(
+                        width: 4,
+                      ),
+                      Flexible(
+                        child: Text(
+                          builder.text.toString(),
+                          style: TextStyle(color: Colors.black),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
           ),
+          // color: Colors.purple,
+          margin: EdgeInsets.only(left: x, top: builder.y),
         ),
       ),
     );
+  }
+
+  bool top_b() {
+    return builder.arrowLocation == ArrowLocation.TOP ||
+        builder.arrowLocation == ArrowLocation.BOTTOM;
+  }
+
+  /// position求的是箭头左顶点x相对于气泡left的距离)
+  double calculateArrowPosition(double x) {
+    double result = 0;
+    if (top_b()) {
+      result = builder.x - x - builder.arrowWidth / 2;
+    }
+
+    print("result= $result");
+    print("builder= " + builder.toString());
+
+    return result;
   }
 }
 
 class BubblePainter extends CustomPainter {
   Rect? mRect;
   final path = Path();
-  final mPaint = Paint()..color = Colors.green;
+  final mPaint = Paint()..color = Colors.pink;
   final BubbleBuilder builder;
 
   BubblePainter(this.builder);
@@ -126,12 +252,19 @@ class BubblePainter extends CustomPainter {
   void setup(Canvas canvas, Size size) {
     mRect ??=
         new Rect.fromLTRB(0, 0, builder.bubbleWidth, builder.bubbleHeight);
+    switch (builder.arrowLocation) {
+      case ArrowLocation.TOP:
+        setUpTopPath(mRect!);
+        break;
+      case ArrowLocation.BOTTOM:
+        setUpBottomPath(mRect!);
+        break;
+    }
 
-    setUpPath(mRect!);
     canvas.drawPath(path, mPaint);
   }
 
-  void setUpPath(Rect rect) {
+  void setUpTopPath(Rect rect) {
     var arrowPosition = builder.arrowPosition;
     path.moveTo(rect.left, rect.top + builder.arrowHeight);
     path.lineTo(rect.left + arrowPosition, rect.top + builder.arrowHeight);
@@ -143,9 +276,27 @@ class BubblePainter extends CustomPainter {
         rect.right, rect.bottom, Radius.circular(builder.radius)));
     path.close();
   }
+
+  void setUpBottomPath(Rect rect) {
+    var arrowPosition = builder.arrowPosition;
+    path.moveTo(rect.left + arrowPosition + builder.arrowWidth,
+        rect.bottom - builder.arrowHeight);
+    path.lineTo(rect.left + arrowPosition + builder.arrowWidth,
+        rect.bottom - builder.arrowHeight);
+    path.lineTo(
+        rect.left + arrowPosition + builder.arrowWidth / 2, rect.bottom);
+    path.lineTo(rect.left + arrowPosition, rect.bottom - builder.arrowHeight);
+
+    path.addRRect(RRect.fromLTRBR(rect.left, rect.top, rect.right,
+        rect.bottom - builder.arrowHeight, Radius.circular(builder.radius)));
+    path.close();
+  }
 }
 
 class BubbleBuilder {
+  static const BUBBLE_WIDTH = 120.0;
+  static const BUBBLE_HEIGHT = 60.0;
+
   //气泡矩形尺寸
   final double bubbleWidth;
   final double bubbleHeight;
@@ -158,16 +309,18 @@ class BubbleBuilder {
   final arrowHeight = 10.0;
 
   final arrowLocation;
-  final String? text;
   final radius;
-  final arrowPosition = 54.0;
+  var arrowPosition = 54.0;
+  String? text;
 
   BubbleBuilder(this.x, this.y,
       {this.arrowLocation = ArrowLocation.TOP,
       this.text,
-      this.bubbleWidth = 120.0,
-      this.bubbleHeight = 60.0})
-      : radius = 4.toDouble();
+      this.bubbleWidth = BUBBLE_WIDTH,
+      this.bubbleHeight = BUBBLE_HEIGHT})
+      : radius = 4.toDouble() {
+    text ??= "simple";
+  }
 }
 
 enum ArrowLocation {
@@ -177,4 +330,5 @@ enum ArrowLocation {
   BOTTOM,
 }
 
-// class BubblePinter
+/// 被点击的目标偏离左边还是右边或是正好
+enum WidgetLocation { left, center, right }
